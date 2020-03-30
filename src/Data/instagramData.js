e2b12ee1d34664913530b47c6b04980d305aeede
Emmaccen <oriolaemmanuel1199@gmail.com>
2020-03-30 14:46:40 +0100

    links
    Changed links to work with deploration build file changes

--- src/Data/instagramData.js
+++ src/Data/instagramData.js
@@ -1,10 +1,10 @@
 const instaData = [
-  {post : '/gallery/5.jpg'},
-  {post : '/gallery/13.jpg'},
-  {post : '/gallery/3.jpg'},
-  {post : '/gallery/7.jpg'},
-  {post : '/gallery/6.jpg'},
-  {post : '/gallery/15.jpg'}
+  {post : './gallery/5.jpg'},
+  {post : './gallery/13.jpg'},
+  {post : './gallery/3.jpg'},
+  {post : './gallery/7.jpg'},
+  {post : './gallery/6.jpg'},
+  {post : './gallery/15.jpg'}
 ]
 
 export default instaData
