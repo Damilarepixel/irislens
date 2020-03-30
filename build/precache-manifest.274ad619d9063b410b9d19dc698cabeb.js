ce8cec97e5404d8534f113697bb6f1dd356b0495
Emmanuel <58217705+Emmaccen@users.noreply.github.com>
2020-03-30 18:40:08 +0100

    Add files via upload

---
+++ build/precache-manifest.274ad619d9063b410b9d19dc698cabeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342d128989828fbc354271b5a4b2d555",
+    "url": "/iris/index.html"
+  },
+  {
+    "revision": "603bca2ad2c23cb35a01",
+    "url": "/iris/static/css/main.ce938bfa.chunk.css"
+  },
+  {
+    "revision": "222721d8182aea7c42d5",
+    "url": "/iris/static/js/2.9f568fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iris/static/js/2.9f568fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603bca2ad2c23cb35a01",
+    "url": "/iris/static/js/main.16c029bb.chunk.js"
+  },
+  {
+    "revision": "55c3212311d46acfb0bd",
+    "url": "/iris/static/js/runtime-main.33c5306e.js"
+  },
+  {
+    "revision": "4dec8e3b54ca2c7542e7718034ebf68e",
+    "url": "/iris/static/media/authorImg.4dec8e3b.jpg"
+  },
+  {
+    "revision": "0aadeed5dd99ee6f80f9b91355b7ff39",
+    "url": "/iris/static/media/homeBg.0aadeed5.jpg"
+  }
+]);
